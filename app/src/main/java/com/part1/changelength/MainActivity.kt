a4549415f99de82ddedec889429218dd0a46684c
mrgoga@naver.com <mrgoga@naver.com>
2023-04-24 21:01:03 +0900

    almost

--- app/src/main/java/com/part1/changelength/MainActivity.kt
+++ app/src/main/java/com/part1/changelength/MainActivity.kt
@@ -2,10 +2,52 @@ package com.part1.changelength
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.widget.addTextChangedListener
+import com.part1.changelength.databinding.ActivityMainBinding
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val outputTextView = binding.outputTextView
+        val inputTextViewParam = binding.inputEditTextParams
+        val inputTextView = binding.inputEditText
+        val outputTextViewReturn = binding.outputEditTextReturn
+        val btnSwap = binding.swapImageBtn
+
+        var inputNumber : Int = 0
+        var cmToMeter = true
+        inputTextView.addTextChangedListener { text ->
+            inputNumber = if(text.isNullOrEmpty()){
+                0
+            } else{
+                text.toString().toInt()
+            }
+            if (cmToMeter){
+                outputTextView.text = inputNumber.times(0.01).toString()
+            } else {
+                outputTextView.text = inputNumber.times(100).toString()
+            }
+        }
+        btnSwap.setOnClickListener {
+            cmToMeter = cmToMeter.not()
+            if (cmToMeter){
+                inputTextViewParam.text = "cm"
+                outputTextViewReturn.text = "m"
+                outputTextView.text = inputNumber.times(0.01).toString()
+            }
+            else{
+                inputTextViewParam.text = "m"
+                outputTextViewReturn.text = "cm"
+                outputTextView.text = inputNumber.times(100).toString()
+            }
+        }
+
     }
 }
